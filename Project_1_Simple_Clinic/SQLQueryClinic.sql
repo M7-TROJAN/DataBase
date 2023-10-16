45d3a5527d03d28d7cf856701813328017f7b6eb
Mahmoud Mohamed <matterm74m7@gmail.com>
2023-10-17 01:05:41 +0300

    Update SQLQueryClinic.sql

--- Project_1_Simple_Clinic/SQLQueryClinic.sql
+++ Project_1_Simple_Clinic/SQLQueryClinic.sql
@@ -11,13 +11,15 @@ CREATE TABLE Persons (
     Phone CHAR(11) CHECK (ISNUMERIC(Phone) = 1), -- Ensure it's numeric
     Email NVARCHAR(200) CHECK (Email LIKE '%@%'), -- Basic email format check
     Address NVARCHAR(200),
-    CONSTRAINT UC_Name UNIQUE (Name) -- Ensure Name is unique
+    CONSTRAINT UC_Name UNIQUE (Name), -- Ensure Name is unique
+	-- Ensure that each record has valid contact information (either a non-null phone or email)
+	CONSTRAINT CHK_ContactInformation CHECK ((Phone IS NOT NULL) OR (Email IS NOT NULL))
 );
 
 -- Create the Doctors table with a foreign key constraint to Persons
 CREATE TABLE Doctors (
     DoctorId INT IDENTITY(1, 1) PRIMARY KEY,
-    PersonId INT UNIQUE, -- Foreign key reference to Persons table's PersonId.
+    PersonId INT UNIQUE NOT NULL, -- Foreign key reference to Persons table's PersonId.
 	Specialization nvarchar(50) not null,
     CONSTRAINT FK_Doctor_Person FOREIGN KEY (PersonId) REFERENCES Persons(PersonId)
 );
@@ -25,7 +27,7 @@ CREATE TABLE Doctors (
 -- Create the Patients table with a foreign key constraint to Persons
 CREATE TABLE Patients (
     PatientId INT IDENTITY(1, 1) PRIMARY KEY,
-    PersonId INT UNIQUE, -- Foreign key reference to Persons table's PersonId.
+    PersonId INT UNIQUE NOT NULL, -- Foreign key reference to Persons table's PersonId.
     CONSTRAINT FK_Patient_Person FOREIGN KEY (PersonId) REFERENCES Persons(PersonId)
 );
 
@@ -44,6 +46,8 @@ VALUES
     (4, 'Canceled'),
     (5, 'Rescheduled'),
     (6, 'No Show');
+
+
 
 -- Create the Payments table to store payment information
 CREATE TABLE Payments (
@@ -65,11 +69,11 @@ CREATE TABLE Medical_Records (
 -- Create the Appointments table with foreign key constraints
 CREATE TABLE Appointments (
     AppointmentId INT IDENTITY(1, 1) PRIMARY KEY,
-    DoctorId INT, -- Foreign key reference to Doctors table's DoctorId.
-    PatientId INT, -- Foreign key reference to Patients table's PatientId.
+    DoctorId INT not null, -- Foreign key reference to Doctors table's DoctorId.
+    PatientId INT not null, -- Foreign key reference to Patients table's PatientId.
     AppointmentDateTime DATETIME NOT NULL, -- Date and time of the appointment
     Appointment_Statu INT, -- Foreign key reference to Appointment_Status table's AppointmentID.
-    PaymentID INT, -- Foreign key reference to Payments table's PaymentID.
+    PaymentID INT not null, -- Foreign key reference to Payments table's PaymentID.
 	MedicalRecord_ID INT, -- Foreign key reference to Medical_Records table's MedicalRecordID.
     CONSTRAINT FK_Doctor_Appointment FOREIGN KEY (DoctorId) REFERENCES Doctors(DoctorId),
     CONSTRAINT FK_Patient_Appointment FOREIGN KEY (PatientId) REFERENCES Patients(PatientId),
@@ -81,7 +85,7 @@ CREATE TABLE Appointments (
 
 CREATE TABLE Prescriptions (
     PrescriptionID INT IDENTITY(1, 1) PRIMARY KEY, 
-    MedicalRecordID INT, -- Foreign key reference to Medical_Records table's MedicalRecordID.
+    MedicalRecordID INT NOT NULL, -- Foreign key reference to Medical_Records table's MedicalRecordID.
     MedicationName NVARCHAR(100) NOT NULL, -- Name of the prescribed medication (required)
     Dosage NVARCHAR(50), -- Dosage information
     Frequency NVARCHAR(50), -- Frequency of medication intake
@@ -91,24 +95,35 @@ CREATE TABLE Prescriptions (
     CONSTRAINT FK_MedicalRecord_Prescription FOREIGN KEY (MedicalRecordID) REFERENCES Medical_Records(MedicalRecordID)
 );
 
-
 -- Trigger for inserting a new doctor
 CREATE TRIGGER PreventDoctorAsPatient
 ON Doctors
 FOR INSERT
 AS
 BEGIN
-    DECLARE @PersonName NVARCHAR(200);
-    SELECT @PersonName = p.Name
-    FROM Persons p
-    WHERE p.PersonId IN (SELECT PersonId FROM inserted);
-
-    IF @PersonName IS NOT NULL
+    -- Check if there is an overlap between the inserted records and existing patients
+    IF EXISTS (
+        SELECT 1 FROM inserted AS i
+        WHERE EXISTS (
+            SELECT 1 FROM Patients AS p
+            WHERE p.PersonId = i.PersonId
+        )
+    )
     BEGIN
-        DECLARE @ErrorMessage NVARCHAR(200);
-        SET @ErrorMessage = 'Person ' + @PersonName + ' is already a patient and cannot be a doctor.';
-        THROW 50000, @ErrorMessage, 1;
-        ROLLBACK TRANSACTION; -- Rollback the insert
+        -- Output a conflict message and the PersonId values involved
+        PRINT 'Conflict Detected:';
+        PRINT 'PersonId values in inserted:';
+        DECLARE @PersonIdInInserted INT;
+        SELECT @PersonIdInInserted = i.PersonId FROM inserted AS i;
+        PRINT @PersonIdInInserted;
+        PRINT 'PersonId values in Patients:';
+        DECLARE @PersonIdInPatients INT;
+        SELECT @PersonIdInPatients = p.PersonId FROM Patients AS p;
+        PRINT @PersonIdInPatients;
+
+        -- Raise an error and roll back the specific INSERT
+        THROW 50000, 'This person is already a patient and cannot be a doctor.', 1;
+        ROLLBACK; -- Roll back the specific INSERT
     END;
 END;
 
@@ -118,20 +133,38 @@ ON Patients
 FOR INSERT
 AS
 BEGIN
-    DECLARE @PersonName NVARCHAR(200);
-    SELECT @PersonName = p.Name
-    FROM Persons p
-    WHERE p.PersonId IN (SELECT PersonId FROM inserted);
-
-    IF @PersonName IS NOT NULL
+    -- Check if there is an overlap between the inserted records and existing doctors
+    IF EXISTS (
+        SELECT 1 FROM inserted AS i
+        WHERE EXISTS (
+            SELECT 1 FROM Doctors AS d
+            WHERE d.PersonId = i.PersonId
+        )
+    )
     BEGIN
-        DECLARE @ErrorMessage NVARCHAR(200);
-        SET @ErrorMessage = 'Person ' + @PersonName + ' is already a doctor and cannot be a patient.';
-        THROW 50000, @ErrorMessage, 1;
-        ROLLBACK TRANSACTION; -- Rollback the insert
+        -- Output a conflict message and the PersonId values involved
+        PRINT 'Conflict Detected:';
+        PRINT 'PersonId values in inserted:';
+        DECLARE @PersonIdInInserted INT;
+        SELECT @PersonIdInInserted = i.PersonId FROM inserted AS i;
+        PRINT @PersonIdInInserted;
+        PRINT 'PersonId values in Doctors:';
+        DECLARE @PersonIdInDoctors INT;
+        SELECT @PersonIdInDoctors = d.PersonId FROM Doctors AS d;
+        PRINT @PersonIdInDoctors;
+
+        -- Raise an error and roll back the specific INSERT
+        THROW 50000, 'This person is already a doctor and cannot be a patient.', 1;
+        ROLLBACK; -- Roll back the specific INSERT
     END;
 END;
 
 
+
 -- If U want to dele a TRIGGER Use The Below Statment
 -- DROP TRIGGER TriggerName;
+
+-- Drop Trigger PreventDoctorAsPatient;
+
+-- Drop Trigger PreventPatientAsDoctor;
+
