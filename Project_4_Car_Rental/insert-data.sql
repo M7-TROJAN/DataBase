9816fecc5d4632861b47d76341e1c24da26c7000
Mahmoud Mohamed <matterm74m7@gmail.com>
2023-10-19 06:00:27 +0300

    Update insert-data.sql

--- Project_4_Car_Rental/insert-data.sql
+++ Project_4_Car_Rental/insert-data.sql
@@ -207,7 +207,7 @@ VALUES (
 	@BookingID, 
 	@ReturnID, 
 	'Payment received for initial booking',  -- Provide more specific information about the payment
-	100.00, 
+	@PaidInitialTotalDueAmount, 
 	@ActualTotalDueAmount, 
 	---- Calculate TotalRemaining
 	CASE
